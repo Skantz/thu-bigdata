62e8b0961d6f503c7ee0edf1bbcc4815a7218cf9
Skantz <danska@kth.se>
2020-04-17 00:50:53 +0900

    hello world, better

--- pagerank-spark/wordcount.py
+++ pagerank-spark/wordcount.py
@@ -1,7 +1,9 @@
 import sys
 import pyspark
 import re
+import time
 
+first = time.time()
 config = pyspark.SparkConf()
 context = pyspark.SparkContext(conf=config)
 
@@ -9,3 +11,5 @@ lines = context.textFile(sys.argv[1])
 lines = lines.flatMap(lambda x: re.split('[^\w]+', x))
 res = lines.map(lambda w: (w, 1)).reduceByKey(lambda x, y: x + y)
 print(res.takeOrdered(10, lambda x: -x[1]))
+last = time.time()
+print("time taken", last - first)
